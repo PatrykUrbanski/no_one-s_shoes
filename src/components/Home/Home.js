91b53f2f3fc9dc69d9e4be35f3ae77b010313a91
Patryk Urbanski <pat.urbanski@gmail.com>
2020-05-18 11:06:50 +0200

    created home_about section

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,6 +3,7 @@ import {Header} from "./header";
 import {Banner} from "./banner";
 import {Home_Columns} from "./home_columns";
 import {Home_instruction} from "./home_instruction";
+import {Home_About} from "./home_about";
 
 export const Home = () => {
     return (
@@ -11,6 +12,7 @@ export const Home = () => {
             <Banner/>
             <Home_Columns/>
             <Home_instruction/>
+            <Home_About/>
         </>
     )
 }
---
+++ src/components/Home/home_about.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export const Home_About = () => {
+    return (
+        <>
+            <section className={"about"}>
+                <div className={"about__text"}>
+                    <div className={"about__text__content"}>
+                        <h2 className={"title"}>About us</h2>
+                        <img src="../../assets/Decoration.svg"/>
+                        <p className={"info"}>Lorem ipsum dolor sit tis dolorem eos, exercitationem fugit harum inventore iste, laboriosam mollitia nam odio officiis quae quia quo reprehenderit vero voluptate.</p>
+                    </div>
+                </div>
+                <img className={"about__img"} src="../../assets/People.jpg"/>
+            </section>
+        </>
+    )
+};
