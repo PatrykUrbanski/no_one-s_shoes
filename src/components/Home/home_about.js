fa371325b0e1c4699936644b3ae95ed83e7fdfba
Patryk Urbanski <pat.urbanski@gmail.com>
2020-05-18 12:37:23 +0200

    who_we_help section done

--- src/components/Home/home_about.js
+++ src/components/Home/home_about.js
@@ -7,8 +7,9 @@ export const Home_About = () => {
                 <div className={"about__text"}>
                     <div className={"about__text__content"}>
                         <h2 className={"title"}>About us</h2>
-                        <img src="../../assets/Decoration.svg"/>
+                        <img className={"decoration"} src="../../assets/Decoration.svg"/>
                         <p className={"info"}>Lorem ipsum dolor sit tis dolorem eos, exercitationem fugit harum inventore iste, laboriosam mollitia nam odio officiis quae quia quo reprehenderit vero voluptate.</p>
+                        <img className={"signature"} src="../../assets/Signature.svg"/>
                     </div>
                 </div>
                 <img className={"about__img"} src="../../assets/People.jpg"/>
---
+++ src/components/Home/who_we_help.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export const Who_We_Help = () => {
+    return (
+        <>
+            <section className="whoWeHelp">
+                <div className="whoWeHelp__content container">
+                    <div className={"whoWeHelp__content__headlines"}>
+                        <h2 className={"title"}>Who we help?</h2>
+                        <img className={"decoration"} src="../../assets/Decoration.svg"/>
+                        <div className={"btn"}>
+                            <a className={"btn__elem"} href={"#"}>elo</a>
+                            <a className={"btn__elem"} href={"#"}>elo</a>
+                            <a className={"btn__elem"} href={"#"}>elo</a>
+                        </div>
+                        <p className={"text"}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt dolorum eos illum ipsum laudantium modi molestiae mollitia necessitatibus non, obcaecati officia optio perferendis perspiciatis praesentium quibusdam recusandae, reiciendis rerum voluptate?</p>
+                    </div>
+                </div>
+            </section>
+        </>
+    )
+}
